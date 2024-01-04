76fe778abf2dcda4fcf849e28548f1f38e009cf4
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2024-01-04 18:25:28 +0300

    First commit

---
+++ src/main/java/com/dobraccon/painmarket/PainMarketApplication.java
@@ -0,0 +1,13 @@
+package com.dobraccon.painmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dobraccon/painmarket/PainMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobraccon.painmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
