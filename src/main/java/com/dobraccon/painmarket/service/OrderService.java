2bdf3cbdd9a70a311be83c4d92ba4dc4960c6727
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2024-01-09 20:07:27 +0300

    P2C-853 Implement order creation API

---
+++ src/main/java/com/dobraccon/painmarket/service/OrderService.java
@@ -0,0 +1,16 @@
+package com.dobraccon.painmarket.service;
+
+import com.dobraccon.painmarket.product.Order;
+import com.dobraccon.painmarket.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class OrderService {
+    private OrderRepository repository;
+
+    public void createOrder(Order order) {
+        repository.createOrder(order);
+    }
+}
