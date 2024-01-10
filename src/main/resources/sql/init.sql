7433bfe01b9df35de57fa05f55f2854104d2770c
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2024-01-10 20:10:36 +0300

    P2C-923 Save all SQL scripts

---
+++ src/main/resources/sql/init.sql
@@ -0,0 +1,57 @@
+CREATE TABLE IF NOT EXISTS product(
+                                      id BIGINT PRIMARY KEY NOT NULL,
+                                      name VARCHAR(128) NOT NULL,
+                                      price REAL NOT NULL,
+                                      discount INT NOT NULL DEFAULT 0
+);
+
+CREATE SEQUENCE IF NOT EXISTS product_sequence START 101;
+
+INSERT INTO product(id, name, price, discount) VALUES(nextval('product_sequence'), 'Трусы', 100.00, 10);
+INSERT INTO product(id, name, price, discount) VALUES(nextval('product_sequence'), 'Носки', 200.00, 20);
+INSERT INTO product(id, name, price, discount) VALUES(nextval('product_sequence'), 'Майка', 300.00, 30);
+INSERT INTO product(id, name, price, discount) VALUES(nextval('product_sequence'), 'Каска', 400.00, 40);
+INSERT INTO product(id, name, price, discount) VALUES(nextval('product_sequence'), 'Пулемёт', 500.00, 50);
+
+CREATE TABLE IF NOT EXISTS customer(
+                                       id BIGINT PRIMARY KEY NOT NULL,
+                                       email VARCHAR(256) NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS customer_sequence START 101;
+
+INSERT INTO customer(id, email) VALUES(nextval('customer_sequence'), 'ivanov@mail.com');
+INSERT INTO customer(id, email) VALUES(nextval('customer_sequence'), 'petrov@mail.com');
+INSERT INTO customer(id, email) VALUES(nextval('customer_sequence'), 'sidorov@mail.com');
+INSERT INTO customer(id, email) VALUES(nextval('customer_sequence'), 'pupkin@mail.com');
+INSERT INTO customer(id, email) VALUES(nextval('customer_sequence'), 'mari_vana@mail.com');
+
+CREATE TABLE IF NOT EXISTS orders(
+                                     id BIGSERIAL PRIMARY KEY NOT NULL,
+                                     product_id BIGSERIAL NOT NULL REFERENCES product(id),
+                                     client_id BIGSERIAL NOT NULL REFERENCES customer(id),
+                                     price BIGSERIAL NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS orders_sequence START 101;
+
+INSERT INTO orders(id, product_id, client_id, price) VALUES(nextval('orders_sequence'), 101, 101, 100);
+INSERT INTO orders(id, product_id, client_id, price) VALUES(nextval('orders_sequence'), 102, 102, 200);
+INSERT INTO orders(id, product_id, client_id, price) VALUES(nextval('orders_sequence'), 103, 103, 300);
+INSERT INTO orders(id, product_id, client_id, price) VALUES(nextval('orders_sequence'), 104, 104, 400);
+INSERT INTO orders(id, product_id, client_id, price) VALUES(nextval('orders_sequence'), 105, 105, 500);
+
+CREATE TABLE IF NOT EXISTS delivery(
+                                       id BIGSERIAL PRIMARY KEY NOT NULL,
+                                       order_id BIGSERIAL NOT NULL REFERENCES orders(id),
+                                       customer_id BIGSERIAL NOT NULL REFERENCES customer(id),
+                                       address VARCHAR(256) NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS delivery_sequence START 101;
+
+INSERT INTO delivery(id, order_id, customer_id, address) VALUES(nextval('delivery_sequence'), 101, 101, 'Рябиновая');
+INSERT INTO delivery(id, order_id, customer_id, address) VALUES(nextval('delivery_sequence'), 102, 102, 'Малиновая');
+INSERT INTO delivery(id, order_id, customer_id, address) VALUES(nextval('delivery_sequence'), 103, 103, 'Клубничная');
+INSERT INTO delivery(id, order_id, customer_id, address) VALUES(nextval('delivery_sequence'), 104, 104, 'Земляничная');
+INSERT INTO delivery(id, order_id, customer_id, address) VALUES(nextval('delivery_sequence'), 105, 105, 'Виноградная');
